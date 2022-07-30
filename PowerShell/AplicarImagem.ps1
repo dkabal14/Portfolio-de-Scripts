ce20b029d3bef31264ad7019530434210ea89088
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-07-29 22:07:31 -0300

    Update AplicarImagem.ps1

--- PowerShell/AplicarImagem.ps1
+++ PowerShell/AplicarImagem.ps1
@@ -1,4 +1,5 @@
-﻿$sig = '
+#Esse script não foi completado e está com alguns bugs no WinPE, tanto x64 quanto x86
+$sig = '
 [DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 [DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);'
 $type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
