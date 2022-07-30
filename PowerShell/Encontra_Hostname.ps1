f43390106ae4151687b90e8d7584632464e7a45b
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-07-29 23:18:57 -0300

    Update Encontra_Hostname.ps1

--- PowerShell/Encontra_Hostname.ps1
+++ PowerShell/Encontra_Hostname.ps1
@@ -1,6 +1,8 @@
-﻿$lista = Get-Content -Path D:\Encontra_Hostname_Lista.txt
+#Encontra o hostname completo através de uma parte dele
+#Precisa do RSAT para funcionar
+$lista = Get-Content -Path D:\Encontra_Hostname_Lista.txt
 foreach ($etiquetaDeTI in $lista)
 {
-    $filtro = 'Name -like "*' + $etiquetaDeTI + '"'
+    $filtro = 'Name -like "*' + $etiquetaDeTI + '*"'
     (Get-ADComputer -Filter $filtro).Name
 }
