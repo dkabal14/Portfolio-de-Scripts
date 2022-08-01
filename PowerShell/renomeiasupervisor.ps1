54f5e2558df916f19a57c79d0053bbcc525a13a8
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-08-01 16:19:40 -0300

    Update renomeiasupervisor.ps1

--- PowerShell/renomeiasupervisor.ps1
+++ PowerShell/renomeiasupervisor.ps1
@@ -1,6 +1,10 @@
-$Supervisor = Get-LocalUser -Name supervisor
-$Builtin = $localUsers | Select-Object -Property * | Where-Object {$_.SID -like "*-500"}
-$Senha_Administrador = ConvertTo-SecureString "#SUPPORT13s123" -AsPlainText -Force
+#Script escrito por diegorosariosousa@gmail.com
+#Script feito para trocar o nome do usuário Administrador built-in (criado com a instalação do Windows) para Supervisor
+#Esse script troca qualquer usuário com o nome "supervisor" para Administrador no lugar dele, e caso esse não exista, ele o cria como um Administrador "fake"
+
+$Supervisor = Get-LocalUser -Name supservisor #Verifica se existe um usuário com nome Supervisor
+$Builtin = $localUsers | Select-Object -Property * | Where-Object {$_.SID -like "*-500"} #Armazena o objeto do usuário built-in
+$Senha_Administrador = ConvertTo-SecureString "#SUPPORT13s123" -AsPlainText -Force #Define a senha do usuário built-in
 if ($Builtin.Name -like "Admin*")
 {
     if ($Supervisor -ne $null) #O Built-in está com nome Admin e o supervisor existe
