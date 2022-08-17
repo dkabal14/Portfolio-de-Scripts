043555247de6abb2ec9e0819d749c07a0ebf583e
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-08-17 17:45:32 -0300

    Transformado em função

--- PowerShell/Criar-CertificadoSha1.ps1
+++ PowerShell/Criar-CertificadoSha1.ps1
@@ -1,6 +1,24 @@
-$email = read-host("Digite o e-mail que codificará o certificado")
-{
-    [Version]
+#Criado por Diego Rosário Sousa a partir do documento: https://kamranicus.com/wmf5-powershell-dsc-generating-encryption-certificate/
+function Criar-CertificadoSha1 {
+    [CmdletBinding()]
+    param (
+        [Parameter(mandatory=$false)]
+        [string]
+        $cerOutput="c:\SHA1.cer",
+        [Parameter(mandatory=$true)]
+        [string]
+        $email,
+        [Parameter(mandatory=$false)]
+        [validateset("Years","Months","Days","Hours","Minutes","Seconds")]
+        [string]
+        $ValidityPeriod="Years",
+        [Parameter(mandatory=$false)]
+        [string]
+        $ValidityPeriodUnity="10"
+    )
+    $infOutput="c:\DocumentEncryption.inf"
+
+    $infInput='    [Version]
     Signature = "$Windows NT$"
     
     [Strings]
@@ -8,7 +26,7 @@ $email = read-host("Digite o e-mail que codificará o certificado")
     szOID_DOCUMENT_ENCRYPTION = "1.3.6.1.4.1.311.80.1"
     
     [NewRequest]
-    Subject = "cn=$($email)"
+    Subject = "cn='+ $email + '"
     MachineKeySet = false
     KeyLength = 2048
     KeySpec = AT_KEYEXCHANGE
@@ -16,9 +34,14 @@ $email = read-host("Digite o e-mail que codificará o certificado")
     Exportable = true
     RequestType = Cert
     KeyUsage = "CERT_KEY_ENCIPHERMENT_KEY_USAGE | CERT_DATA_ENCIPHERMENT_KEY_USAGE"
-    ValidityPeriod = "Years"
-    ValidityPeriodUnits = "1000"
+    ValidityPeriod = ' + $ValidityPeriod + '
+    ValidityPeriodUnits = ' + $ValidityPeriodUnity + '
     
     [Extensions]
-    %szOID_ENHANCED_KEY_USAGE% = "{text}%szOID_DOCUMENT_ENCRYPTION%"
-} | Out-File -FilePath d:\DocumentEncryption.inf
+    %szOID_ENHANCED_KEY_USAGE% = "{text}%szOID_DOCUMENT_ENCRYPTION%"'
+
+    $infInput | Out-File -FilePath $infOutput
+    
+    certreq -new $infOutput $cerOutput
+}
+Criar-CertificadoSha1 -cerOutput "c:\CertificadoSHA1.cer" -email "diegorosariosousa@gmail.com" -ValidityPeriod "Years" -ValidityPeriodUnity "10"
