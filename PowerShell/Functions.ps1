7137c3e0a8d8e36ea867da1ee7a3622c0bd4b804
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-08-17 17:49:33 -0300

    O update que não funcionou pelo VSCode

--- PowerShell/Functions.ps1
+++ PowerShell/Functions.ps1
@@ -345,3 +345,48 @@ function Popup {
         }
     }
 }
+function Criar-CertificadoSha1 {
+    [CmdletBinding()]
+    param (
+        [Parameter(mandatory=$false)]
+        [string]
+        $cerOutput="c:\SHA1.cer",
+        [Parameter(mandatory=$true)]
+        [string]
+        $email,
+        [Parameter(mandatory=$false)]
+        [validateset("Years","Months","Days","Hours","Minutes","Seconds")]
+        [string]
+        $ValidityPeriod="Years",
+        [Parameter(mandatory=$false)]
+        [string]
+        $ValidityPeriodUnity="10"
+    )
+    $infOutput="c:\DocumentEncryption.inf"
+
+    $infInput='    [Version]
+    Signature = "$Windows NT$"
+    
+    [Strings]
+    szOID_ENHANCED_KEY_USAGE = "2.5.29.37"
+    szOID_DOCUMENT_ENCRYPTION = "1.3.6.1.4.1.311.80.1"
+    
+    [NewRequest]
+    Subject = "cn='+ $email + '"
+    MachineKeySet = false
+    KeyLength = 2048
+    KeySpec = AT_KEYEXCHANGE
+    HashAlgorithm = Sha1
+    Exportable = true
+    RequestType = Cert
+    KeyUsage = "CERT_KEY_ENCIPHERMENT_KEY_USAGE | CERT_DATA_ENCIPHERMENT_KEY_USAGE"
+    ValidityPeriod = ' + $ValidityPeriod + '
+    ValidityPeriodUnits = ' + $ValidityPeriodUnity + '
+    
+    [Extensions]
+    %szOID_ENHANCED_KEY_USAGE% = "{text}%szOID_DOCUMENT_ENCRYPTION%"'
+
+    $infInput | Out-File -FilePath $infOutput
+    
+    certreq -new $infOutput $cerOutput
+} 
