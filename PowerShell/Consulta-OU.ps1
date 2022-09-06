a0416a8dbc98a5c0a66c944a75049d40c8c7dd36
dkabal14 <58339775+dkabal14@users.noreply.github.com>
2022-09-06 18:10:41 -0300

    Observações

--- PowerShell/Consulta-OU.ps1
+++ PowerShell/Consulta-OU.ps1
@@ -1,5 +1,6 @@
 #O script mostra em qual Unidade Organizacional o objeto do equipamento se encontra no Active Directory
 #sem a necessidade de instalação do RSAT.
+#Get-WMIObject deixa de funcionar no Windows 11 e em versões mais novas do powershell (6 ou superior), é preciso levar em consideração que a porta de comunicação do Get-CimClass é diferente da do cmdlet Get-WMIObject
 $sair = "nao"
 Do
 {
