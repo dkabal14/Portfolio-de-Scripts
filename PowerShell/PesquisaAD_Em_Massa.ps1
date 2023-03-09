8ca968f5a0430af3d300bc0979b0daaf26a6823c
Diego R <58339775+dkabal14@users.noreply.github.com>
2023-03-09 00:43:11 -0300

    Atualizando informações do script

--- PowerShell/PesquisaAD_Em_Massa.ps1
+++ PowerShell/PesquisaAD_Em_Massa.ps1
@@ -1,13 +1,14 @@
-﻿#Script escrito por Diego Rosário Sousa - Suporte Nível 3 RJ
+﻿#Script escrito por Diego Rosário Sousa
 #diegorosariosousa@gmail.com
 
 $Credenciais = Get-Credential
 $Computers = Get-Content -Path $PSScriptRoot\MoveAD_Em_Massa_Lista.txt
+$server = "contoso.net.us"
 
 Foreach ($Computer in $Computers)
 {
     if ($Computer -notlike "#*")
     {
-        Get-ADComputer -Filter {Name -eq $Computer} -Credential $Credenciais -Properties * -server oi.corp.net -SearchScope Subtree -SearchBase 'OU=Estacoes,DC=oi,DC=corp,DC=net' -OutVariable $teste | Select-Object -Property Name,CanonicalName
+        Get-ADComputer -Filter {Name -eq $Computer} -Credential $Credenciais -Properties * -server $server -SearchScope Subtree -SearchBase 'OU=Stations,DC=contoso,DC=net,DC=us' -OutVariable $teste | Select-Object -Property Name,CanonicalName
     }
 }
