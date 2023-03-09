a88688852bc6540372a9165a29ca765f9e891a50
Diego R <58339775+dkabal14@users.noreply.github.com>
2023-03-09 19:03:16 -0300

    testing filter-quotes

--- PowerShell/Teste.ps1
+++ PowerShell/Teste.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿$TruthA = @(0, 0, 0, 0, 1, 1, 1, 1)
+$TruthB = @(0, 0, 1, 1, 0, 0, 1, 1)
+$TruthC = @(0, 1, 0, 1, 0, 1, 0, 1)
+
+$TruthTable = 0..7| foreach {
+    [PSCustomObject]@{
+        chkAuthor = $TruthA[$_]
+        chkTags = $TruthB[$_]
+        chkPage = $TruthC[$_]
+        Case = $_
+    }
+}
+
+$Case = ($TruthTable | Where-Object {$_.chkAuthor -eq $chkAuthor -and $_.chkTags -eq $chkTags -and $_.chkPage -eq $chkPage}).Case
