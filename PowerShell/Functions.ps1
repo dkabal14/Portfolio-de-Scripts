115a3f9bbb702494ab877a4f4ee9fb17a8690364
Diego R <58339775+dkabal14@users.noreply.github.com>
2023-03-10 15:31:46 -0300

    added the function Get-CredentialGUI that shows a form for inputting username and password on applications without securing the password string.

--- PowerShell/Functions.ps1
+++ PowerShell/Functions.ps1
@@ -1,5 +1,64 @@
+#Used for Get-CredentialGUI() and input-grafico()
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
+
+function Get-CredentialGUI
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Titulo
+    )
+    $inputForm = New-Object System.Windows.Forms.Form
+    $inputForm.Text = $Titulo
+    $inputForm.Size = New-Object System.Drawing.Size(350,200)
+    $inputForm.StartPosition = 'CenterScreen'
+
+    $lb = New-Object System.Windows.Forms.Label
+    $lb.Location = New-Object System.Drawing.Point(20,20)
+    $lb.Size = New-Object System.Drawing.Size(240,20)
+    $lb.Text = "E-mail:"
+    $inputForm.Controls.Add($lb)
+
+    $tb1 = New-Object System.Windows.Forms.TextBox
+    $tb1.Location = New-Object System.Drawing.Point(20,40)
+    $tb1.Size = New-Object System.Drawing.Size(240,20)
+    $inputForm.Controls.Add($tb1)
+
+    $lb2 = New-Object System.Windows.Forms.Label
+    $lb2.Location = New-Object System.Drawing.Point(20,60)
+    $lb2.Size = New-Object System.Drawing.Size(240,20)
+    $lb2.Text = "Senha"
+    $inputForm.Controls.Add($lb2)
+
+    $tb2 = New-Object System.Windows.Forms.TextBox
+    $tb2.Location = New-Object System.Drawing.Point(20,80)
+    $tb2.Size = New-Object System.Drawing.Size(240,20)
+    $tb2.PasswordChar = '#'
+    $inputForm.Controls.Add($tb2)
+
+    $okb = New-Object System.Windows.Forms.Button
+    $okb.Location = New-Object System.Drawing.Point(20,105)
+    $okb.Size = New-Object System.Drawing.Size(75,25)
+    $okb.Text = 'Ok'
+    $okb.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $inputForm.AcceptButton = $okb
+    $inputForm.Controls.Add($okb)
+
+    $inputForm.Topmost = $true
+    $inputForm.Add_Shown({$tb1.Select()})
+    $rs = $inputForm.ShowDialog()
+    if ($rs -eq [System.Windows.Forms.DialogResult]::OK)
+    {
+        $result = [PSCustomObject]@{
+            email = $tb1.text
+            senha = $tb2.text
+        }
+        return $result
+    }
+}
+
 function input-grafico()
 {
     [CmdletBinding()]
