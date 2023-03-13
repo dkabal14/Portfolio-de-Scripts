2fdbae2720e2814f637885c7936c5cc0a84e0bac
Diego R <58339775+dkabal14@users.noreply.github.com>
2023-03-13 14:44:37 -0300

    Teste d Função GetCredential-GUI(insecure)

--- PowerShell/Teste.ps1
+++ PowerShell/Teste.ps1
@@ -1,14 +1,60 @@
-﻿$TruthA = @(0, 0, 0, 0, 1, 1, 1, 1)
-$TruthB = @(0, 0, 1, 1, 0, 0, 1, 1)
-$TruthC = @(0, 1, 0, 1, 0, 1, 0, 1)
+﻿#Teste Get-CredentialGUI
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+function Get-CredentialGUI
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Titulo
+    )
+    $inputForm = New-Object System.Windows.Forms.Form
+    $inputForm.Text = $Titulo
+    $inputForm.Size = New-Object System.Drawing.Size(350,200)
+    $inputForm.StartPosition = 'CenterScreen'
+    $inputForm.Icon = ('C:\d\Scripts\Github\Portfolio-de-Scripts\PowerShell\Hihgbond-LaunchPadModule\Resources\Highbond-icon.ico')
 
-$TruthTable = 0..7| foreach {
-    [PSCustomObject]@{
-        chkAuthor = $TruthA[$_]
-        chkTags = $TruthB[$_]
-        chkPage = $TruthC[$_]
-        Case = $_
+    $lb = New-Object System.Windows.Forms.Label
+    $lb.Location = New-Object System.Drawing.Point(20,20)
+    $lb.Size = New-Object System.Drawing.Size(240,20)
+    $lb.Text = "E-mail:"
+    $inputForm.Controls.Add($lb)
+
+    $tb1 = New-Object System.Windows.Forms.TextBox
+    $tb1.Location = New-Object System.Drawing.Point(20,40)
+    $tb1.Size = New-Object System.Drawing.Size(240,20)
+    $inputForm.Controls.Add($tb1)
+
+    $lb2 = New-Object System.Windows.Forms.Label
+    $lb2.Location = New-Object System.Drawing.Point(20,60)
+    $lb2.Size = New-Object System.Drawing.Size(240,20)
+    $lb2.Text = "Senha"
+    $inputForm.Controls.Add($lb2)
+
+    $tb2 = New-Object System.Windows.Forms.TextBox
+    $tb2.Location = New-Object System.Drawing.Point(20,80)
+    $tb2.Size = New-Object System.Drawing.Size(240,20)
+    $tb2.PasswordChar = '#'
+    $inputForm.Controls.Add($tb2)
+
+    $okb = New-Object System.Windows.Forms.Button
+    $okb.Location = New-Object System.Drawing.Point(20,105)
+    $okb.Size = New-Object System.Drawing.Size(75,25)
+    $okb.Text = 'Ok'
+    $okb.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $inputForm.AcceptButton = $okb
+    $inputForm.Controls.Add($okb)
+
+    $inputForm.Topmost = $true
+    $inputForm.Add_Shown({$tb1.Select()})
+    $rs = $inputForm.ShowDialog()
+    if ($rs -eq [System.Windows.Forms.DialogResult]::OK)
+    {
+        $result = [PSCustomObject]@{
+            email = $tb1.text
+            senha = (Convertto-Securestring $tb2.text -AsPlaintext -Force)
+        }
+        return $result
     }
 }
-
-$Case = ($TruthTable | Where-Object {$_.chkAuthor -eq $chkAuthor -and $_.chkTags -eq $chkTags -and $_.chkPage -eq $chkPage}).Case
